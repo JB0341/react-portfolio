b6f0583cac7247956dca8189da968b2128a23434
j b <j.is.brown@gmail.com>
2024-10-17 16:24:22 -0600

    resume logic written

---
+++ vite-project/src/pages/Resume.jsx
@@ -0,0 +1,23 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume under revision! Go to the contact page to request a copy to be sent once it is finished! </h1>
+            <ul>
+                <h1>Front end proficiencies</h1>
+                <li>Html</li>
+                <li>Css</li>
+                <li>React</li>
+                <li>JavaScript</li>
+            </ul>
+
+            <ul>
+                <h1> Back end proficiencies</h1>
+                <li>JavaScript</li>
+                <li>Express.js</li>
+                <li>SQL</li>
+                <li>NOSQL</li>
+
+            </ul>
+        </div>
+    )
+}
