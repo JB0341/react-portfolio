11f823909019ff180532123adaff2182aebf6059
j b <j.is.brown@gmail.com>
2024-10-17 20:36:51 -0600

    link added for projects in portfolio

--- vite-project/src/pages/Portfolio.jsx
+++ vite-project/src/pages/Portfolio.jsx
@@ -40,9 +40,7 @@ export default function Portfolio() {
                             <div className="card-body">
                                 <h5 className="card-title">{project.title}</h5>
                                 <p className="card-text">{project.description}</p>
-                                <p className="card-text">
-                                    <small className="text-body-secondary">{project.lastUpdated}</small>
-                                </p>
+                                <a href={project.link} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View Project</a>
                             </div>
                         </div>
                     </div>
