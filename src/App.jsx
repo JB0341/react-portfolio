ff4916d6b4306e5dea82cc7ca77be4a459bee2af
j b <j.is.brown@gmail.com>
2024-10-17 21:25:07 -0600

    importing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';  
 
